8c990decde480943e5ef929b8b696ffa87a24851
hermesthemes <121753577+HermesThemes@users.noreply.github.com>
2023-08-28 18:28:01 +0100

    Update home.js

--- src/app/components/home.js
+++ src/app/components/home.js
@@ -1,35 +1,8 @@
-import anime from "animejs"
+import styles from './HomeStyle.module.css'
 
 export default function Home(){
 
-  function animeBtn(isthere){
-    // if(isthere){
-    //   anime({
-    //       targets: ".orderbtn",
-          
-    //       duration: 100,
-    //       boxShadow: '0 0 15px var(--primaryColor)',
-    //       easing: 'easeInOutQuad'
-    //   })
-    //   anime({
-    //       targets: ".orderbtn",
-    //       scale: 1.1,
-    //       duration: 500,
-         
-    //       easing: 'easeInOutQuad'
-    //   })
-
-    // }
-    // else{
-    //   anime({
-    //     targets: ".orderbtn",
-    //     scale: 1.0,
-    //     duration: 500,
-    //     boxShadow: '0 0 10px var(--primaryColor)',
-    //     easing: 'easeInOutQuad'
-    // })
-    // }
-  }
+  
     return(
         <div className="home text-center" id="home">
           <main className="">
@@ -39,8 +12,8 @@ export default function Home(){
                   <h1 className="">Welcome to my <span className="glow">Page</span></h1>
                   <h3>I am A full stack web devloper not yet :( but soon</h3>
                   <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit </h3>
-                  <button className="btn btn-light mt-5 mx-3 orderbtn">Order Now!</button>
-                  <button className="btn btn-light mt-5 mx-3 conbtn">Contact Me</button>
+                  <button className={`btn btn-light mt-5 me-3 ${styles.orderbtn}`}>Order Now!</button>
+                  <button className={`btn btn-light mt-5 ms-3 ${styles.conbtn}`}>Contact Me</button>
                 </div>
               </div>
             </div>
