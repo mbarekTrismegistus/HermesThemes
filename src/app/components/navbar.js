98b3d8b34b0e9dc63136daf2cdbc80ac4144d3b6
hermesthemes <121753577+HermesThemes@users.noreply.github.com>
2023-08-28 18:39:10 +0100

    Update navbar.js

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import React from 'react';
+import styles from './NavbarStyle.module.css'
 
 
 
@@ -8,8 +9,8 @@ export default function NavBar(props){
     
     function handleColor(event){
         setColor(event.target.value)
-        document.documentElement.style.setProperty(`--primaryColor`, `${event.target.value}`);
-        document.documentElement.style.setProperty(`--transparentColor`, `${event.target.value}d8`);
+        document.documentElement.style.setProperty(`--primaryColor`, `${color}`);
+        document.documentElement.style.setProperty(`--transparentColor`, `${color}d8`);
         
         
     }
@@ -20,10 +21,10 @@ export default function NavBar(props){
 
     return(
         <header>
-            <nav className={`navbar navbar-expand-lg navbar-${props.theme.color} fixed-top`}>
+            <nav className={`navbar navbar-expand-lg navbar-${props.theme.color} bg-${props.theme.color} fixed-top`}>
                 <div className="container-fluid mt-0">
-                <a href="#" className={`navbar-brand text-${props.theme.text}`}>
-                    <Image src={`/mercury${props.theme.color}.png`} alt="" width={50} height={50}/>
+                <a href="#" className={`navbar-brand text-${props.theme.text} ${styles.navbarBrand}`}>
+                    <Image src={`/mercury${props.theme.color}.jpg`} alt="" width={50} height={50}/>
                     HermesThemes
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -31,28 +32,28 @@ export default function NavBar(props){
                 </button>
                 <div className="collapse navbar-collapse justify-content-center text-center" id="navbarSupportedContent">
                     <ul className={`nav navbar-nav ms-auto text-white`}>
-                    <li className={`nav-item me-3 ${props.selected === "home" ? "active" : ""}`}>
-                        <button className={`nav-link px-2 text-${props.theme.text}`} onClick={() => props.setComponent("home")}>Home</button>
+                    <li className={`${styles.navitem} me-3 ${props.selected === "home" ? styles.active : ""}`}>
+                        <button className={`${styles.navlink} nav-link px-2 text-${props.theme.text}`} onClick={() => props.setComponent("home")}>Home</button>
                     </li>
-                    <li className={`nav-item me-3 ${props.selected === "about" ? "active" : ""}`}>
-                        <button className={`nav-link px-2 text-${props.theme.text}`} onClick={() => props.setComponent("about")}>About Me</button>
+                    <li className={`${styles.navitem} me-3 ${props.selected === "about" ? styles.active : ""}`}>
+                        <button className={`${styles.navlink} nav-link px-2 text-${props.theme.text}`} onClick={() => props.setComponent("about")}>About Me</button>
                     </li>
-                    <li className={`nav-item me-3 ${props.selected === "product" ? "active" : ""}`}>
-                        <button className={`nav-link px-2 text-${props.theme.text}`} onClick={() => props.setComponent("product")} >Products</button>
+                    <li className={`${styles.navitem} me-3 ${props.selected === "product" ? styles.active : ""}`}>
+                        <button className={`${styles.navlink} nav-link px-2 text-${props.theme.text}`} onClick={() => props.setComponent("product")} >Products</button>
                     </li>
-                    <li className={`nav-item me-3 ${props.selected === "contact" ? "active" : ""}`}>
-                        <button className={`nav-link px-2 text-${props.theme.text}`} onClick={() => props.setComponent("contact")} >Contact Me</button>
+                    <li className={`${styles.navitem} me-3 ${props.selected === "contact" ? styles.active : ""}`}>
+                        <button className={`${styles.navlink} nav-link px-2 text-${props.theme.text}`} onClick={() => props.setComponent("contact")} >Contact Me</button>
                     </li>
-                    <li className={`nav-item me-3 ${props.selected === "others" ? "active" : ""}`}>
-                        <button className={`nav-link px-2 text-${props.theme.text}`} onClick={() => props.setComponent("others")} >Others</button>
+                    <li className={`${styles.navitem} me-3 ${props.selected === "others" ? styles.active : ""}`}>
+                        <button className={`${styles.navlink} nav-link px-2 text-${props.theme.text}`} onClick={() => props.setComponent("others")} >Others</button>
                     </li>
                     </ul>
 
                     <form className="d-flex ms-auto">
                         <input className="form-color me-2 mt-1" onChange={handleColor} value={color} id="color" type="color"/>
                         
-                        <div className="tbtn-con ms-auto me-3" onClick={props.changeTheme}>
-                            <div className="toggle-btn my-1"></div>
+                        <div className={`${styles.tbtncon} ms-auto me-3`} id='tbtncon' onClick={props.changeTheme}>
+                            <div className={`${styles.toggleBtn} my-1`} id='toggleBtn'></div>
                         </div>
                     </form>
                 </div>
