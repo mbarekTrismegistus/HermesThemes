2d23e850885f0801a66515df812f96f29b58bf10
hermesthemes <121753577+HermesThemes@users.noreply.github.com>
2023-08-31 10:45:32 +0000

    Update products.js

--- src/app/components/products.js
+++ src/app/components/products.js
@@ -41,7 +41,7 @@ export default function Products(props){
               
               <div className="col-lg-6 d-flex justify-content-center mt-5">
                 <div className={`${styles.pcard} card mx-5`}>
-                  <Image src="/p1.png" width={500} height={200} className="card-img-top" alt="..."/>
+                  <Image src="/p1.png" width={500} height={200} className="card-img-top img img-fluid" alt="..."/>
                   
                   <div className={`card-body ${styles.cardbody}`}>
                     <h5 className="card-title">My first product</h5>
