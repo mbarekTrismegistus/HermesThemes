b823e1b71ecb4878597611cd9a6fd89ff52d2e84
DWFS202L <momoboogeyman2000@gmail.com>
2023-11-29 10:21:28 +0100

    add files

---
+++ public/script.js
@@ -0,0 +1,126 @@
+
+
+// $(".orderbtn").on('mouseenter',function(){
+//     anime({
+//         targets: this,
+//         scale: 1.1,
+//         duration: 500,
+        
+//         easing: 'easeOutExpo'
+//     })
+// })
+// $(".orderbtn").on('mouseenter',function(){
+//     anime({
+//         targets: this,
+//         duration: 500,
+//         keyframes: [
+//             {boxShadow: '0 0 15px var(--primaryColor)'},
+//             {boxShadow: '0 0 20px var(--primaryColor)'},
+//             {boxShadow: '0 0 25px var(--primaryColor)'},
+//             {boxShadow: '0 0 20px var(--primaryColor)'},
+//             {boxShadow: '0 0 15px var(--primaryColor)'},
+//         ],
+        
+//         easing: 'easeOutExpo'
+//     })
+// })
+// $(".orderbtn").on('mouseleave',function(){
+//     anime({
+//         targets: this,
+        
+//         duration: 500,
+//         boxShadow: '0 0 0px var(--primaryColor)',
+        
+//         easing: 'easeOutExpo'
+//     })
+// })
+// $(".orderbtn").on('mouseleave',function(){
+//     anime({
+//         targets: this,
+//         scale: 1.0,
+//         duration: 500,
+        
+//         easing: 'easeOutExpo'
+//     })
+// })
+// anime({
+//     targets: '.glow',
+    
+//     duration: 2000,
+//     keyframes: [
+//         {textShadow: '0 0 10px var(--primaryColor)'},
+//         {textShadow: '0 0 30px var(--primaryColor)'},
+//         {textShadow: '0 0 10px var(--primaryColor)'}
+        
+        
+//     ],
+//     direction: 'alternate',
+//     loop: true,
+//     easing: 'linear'
+// })
+
+
+
+// $("#About").click(function(){
+//     $('#circlejs').circleProgress({
+//         value: 0.75,
+//         size: 120,
+//         thickness: 10,
+//         fill: {
+//           gradient: ["#f0db4f", "#968618"]
+//         },
+//         lineCap: "round"
+//     });
+//     $('#circlehtml').circleProgress({
+//         value: 0.90,
+//         size: 120,
+//         thickness: 10,
+//         fill: {
+//           gradient: ["#f06529", "#e34c26"]
+//         },
+//         lineCap: "round"
+//     });
+//     $('#circlecss').circleProgress({
+//         value: 0.80,
+//         size: 120,
+//         thickness: 10,
+//         fill: {
+//           gradient: ["#2965f1", "#264de4"]
+//         },
+//         lineCap: "round"
+//     });
+//     $('#circlejq').circleProgress({
+//         value: 0.75,
+//         size: 120,
+//         thickness: 10,
+//         fill: {
+//           gradient: ["#2965f1", "#264de4"]
+//         },
+//         lineCap: "round"
+//     });
+//     $('#circler').circleProgress({
+//         value: 0.75,
+//         size: 120,
+//         thickness: 10,
+//         fill: {
+//           gradient: ["#2965f1", "#264de4"]
+//         },
+//         lineCap: "round"
+//     });
+        
+// })
+
+
+
+
+    
+   
+  
+  
+
+
+
+
+
+
+
---
+++ src/app/components/products.js
@@ -0,0 +1,97 @@
+import Image from "next/image"
+import $ from 'jquery';
+import React from "react";
+import anime from "animejs"
+import styles from './productStyle.module.css'
+
+export default function Products(props){
+
+  React.useEffect(() => {
+
+    $(".card-img-top").on('mouseenter',function(){
+      
+      anime({
+          targets: this,
+          scale: 1.1,
+          duration: 500,
+          backgroundColor: '#FFF',
+          easing: 'easeOutExpo'
+      })
+  })
+  
+  $(".card-img-top").on('mouseleave',function(){
+      anime({
+          targets: this,
+          scale: 1.0,
+          duration: 500,
+          easing: 'easeOutExpo'
+      })
+  })
+  })
+
+
+
+
+    return(
+      <div className="container">
+        <div className={`${styles.product} pt-5 container`} id="products">
+          <h1 className="text-center glow">My Products</h1>
+          <div className="container">
+            <div className="text-center text-white row my-5">
+              
+              <div className="col-lg-6 d-flex justify-content-center mt-5">
+                <div className={`${styles.pcard} card mx-5`}>
+                  <Image src="/p1.png" width={500} height={200} className="card-img-top img img-fluid" alt="..."/>
+                  
+                  <div className={`card-body ${styles.cardbody}`}>
+                    <h5 className="card-title">My first product</h5>
+                    <button className={`${styles.pbtn} btn btn-outline-dark d-block mx-auto my-3`}>Get the theme</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6 d-flex justify-content-center  mt-5">
+                <div className={`${styles.pcard} card mx-5`}>
+                  <Image src="/p3.png" width={500} height={200}  className="card-img-top img img-fluid" alt="..."/>
+                  
+                  <div className={`card-body ${styles.cardbody}`}>
+                    <h5 className="card-title">My first product</h5>
+                    <button className={`${styles.pbtn} btn btn-outline-dark d-block mx-auto my-3`}>Get the theme</button>
+                  </div>
+                </div>
+              </div>
+              
+              
+            </div>
+            <div className="text-center text-white row my-5">
+              
+              <div className="col-lg-6 mt-5 d-flex justify-content-center ">
+                <div className={`${styles.pcard} card mx-5`}>
+                  <Image src="/p3.webp" width={500} height={200} className="card-img-top img img-fluid" alt="..."/>
+                  
+                  <div className={`card-body ${styles.cardbody}`}>
+                    <h5 className="card-title">My first product</h5>
+                    <button className={`${styles.pbtn} btn btn-outline-dark d-block mx-auto my-3`}>Get the theme</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6 mt-5 d-flex justify-content-center ">
+                <div className={`${styles.pcard} card mx-5`}>
+                  <Image src="/p4.png" width={500} height={200} className="card-img-top img img-fluid" alt="..."/>
+                  
+                  <div className={`card-body ${styles.cardbody}`}>
+                    <h5 className="card-title">My first product</h5>
+                    <button className={`${styles.pbtn} btn btn-outline-dark d-block mx-auto my-3`}>Get the theme</button>
+                  </div>
+                </div>
+              </div>
+              
+              
+            </div>
+            
+            
+          </div>
+        </div>
+
+      </div>
+    )
+}
